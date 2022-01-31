dc5dc3f969b2320cecb407fe62ce00b0803613a4
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-31 15:08:17 +0100

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <h1>Work in progress</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {
+
+    }
+}
+</script>
+
+<style language="scss">
+
+</style>
---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Boolpress</title>
+</head>
+<body>
+
+    {{-- APP --}}
+    <div id="root"></div>
+
+    {{-- JS VUE --}}
+    <script src="{{ asset('js/font-end.js') }}"></script>
+</body>
+</html>
