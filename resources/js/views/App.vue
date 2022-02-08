2a4f8a6c33b98674faf2de82ceff6054be462071
Nuccia955 <elenamunaro1995@gmail.com>
2022-02-08 14:29:18 +0100

    connect api endpoint + frontend call with axios

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,36 @@
 <template>
     <div class="main-content py-5">
         <div class="container">
-            <h1 class="text-center">Work in progress</h1>
+            <h1 class="text-uppercase mb-5">Blog class #45</h1>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'App',
     components: {
 
+    },
+    data() {
+        return {
+            post: null,
+            pagination: null,
+        }
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/posts')
+            .then(response => {
+                this.post = response.data.data;
+                console.log(this.post);
+
+                this.pagination = {
+                    current: response.data.current_page,
+                    last: response.data.last_page,
+                };
+                console.log(this.pagination);
+            })
     }
 }
 </script>
