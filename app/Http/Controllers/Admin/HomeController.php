6062c234ce6657e508205b1092a094fec62265be
Nuccia955 <elenamunaro1995@gmail.com>
2022-02-14 15:52:49 +0100

    add static logic for SendEmail + blade

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmail;
 
 class HomeController extends Controller
 {
     public function index() {
+        Mail::to('cicciopasticcio@test.com')->send(new SendEmail());
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/mailtest.blade.php
@@ -0,0 +1,3 @@
+<h1>Welcome User</h1>
+
+<p>This is a test mail!</p>
